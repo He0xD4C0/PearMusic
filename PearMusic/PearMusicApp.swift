01c1d79e3b9408baaf0a89f1c6cd2b0e426ebebc
He0xD4C0 <50360903+hex-0xd4c0@users.noreply.github.com>
2026-06-03 19:45:09 +0800

    Initial Commit

---
+++ PearMusic/PearMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PearMusicApp.swift
+//  PearMusic
+//
+//  Created by He0xD4C0 on 2026/6/3.
+//
+
+import SwiftUI
+
+@main
+struct PearMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
