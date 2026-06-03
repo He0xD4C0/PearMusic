2b95b9932a92c35c50785f023dd52d0082848ace
He0xD4C0 <50360903+hex-0xd4c0@users.noreply.github.com>
2026-06-03 22:02:41 +0800

    fix: PlaybackEngine.currentPlaybackTime -> currentTime

--- PearMusic/Shared/ViewModels/LyricViewModel.swift
+++ PearMusic/Shared/ViewModels/LyricViewModel.swift
@@ -104,7 +104,7 @@ public final class LyricViewModel {
             if machine.shouldSync, let lines = machine.lines {
                 syncer.setLines(lines)
                 syncer.start {
-                    PlaybackEngine.shared.currentPlaybackTime
+                    PlaybackEngine.shared.currentTime
                 }
             }
         } catch {
